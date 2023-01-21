478b046ef7388a64ef7fdb7ebd8f1f5791a34a68
sinri <1lliodqb@gmail.com>
2023-01-22 00:10:04 +0900

    feat: Add Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,55 @@
+import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
 
 import { links } from "routes/links";
 
 const Header = () => {
+  const refDrawer = useRef<HTMLDivElement>(null);
+
   return (
-    <header>
-      <h1>reference typescript cra</h1>
-      <nav>
-        <Link to={links.home}>home</Link>
-        <Link to={links.profile}>profile</Link>
-        <Link to={links.portfolio}>portfolio</Link>
-        <Link to={links.gallery}>gallery</Link>
-      </nav>
+    <header className="header">
+      <div className="header-wrap">
+        <div className="header-title">
+          <h1 className="title">hello sinri!</h1>
+        </div>
+        <div className="header-theme">
+          <button>day/night</button>
+        </div>
 
-      <button>en/ko</button>
+        <div className="header-drawer">
+          <button 
+            className="btn drawer-trigger"
+            onClick={() => {
+              refDrawer.current?.style.display === 'none'
+              ? refDrawer.current.style.setProperty("display", 'block')
+              : refDrawer.current?.style.setProperty("display", 'none')
+            }}
+          >drawer box icon</button>
 
-      <ul className="links">
-        <li>velog</li>
-        <li>github</li>
-        <li>notion</li>
-        <li>codepen</li>
-      </ul>
+          <div 
+            ref={refDrawer}
+            className="drawer-container" 
+          >
+            <div className="drawer-wrap">
+              <nav>
+                <Link to={links.home}>home</Link>
+                <Link to={links.profile}>profile</Link>
+                <Link to={links.portfolio}>portfolio</Link>
+                <Link to={links.gallery}>gallery</Link>
+              </nav>
+
+              <button>en/ko</button>
+
+              <ul className="links">
+                <li>velog</li>
+                <li>github</li>
+                <li>notion</li>
+                <li>codepen</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 };
