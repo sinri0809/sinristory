4db7091bebe7bef19b8c2f126a012f3972ec6c23
sinri <1lliodqb@gmail.com>
2023-01-23 22:25:41 +0900

    feat: Add button & micro-interaction  - look header  - add style module - color, fontSize

---
+++ src/components/icons/IconDrawer.tsx
@@ -0,0 +1,15 @@
+const IconDrawer = () => {
+  return <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect className="rect00" x="7" y="7" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect10" x="7" y="15" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect20" x="7" y="23" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect01" x="15" y="7" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect11" x="15" y="15" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect21" x="15" y="23" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect02" x="23" y="7" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect12" x="23" y="15" width="6" height="6" rx="2" fill="white" />
+    <rect className="rect22" x="23" y="23" width="6" height="6" rx="2" fill="white" />
+  </svg>
+}
+
+export default IconDrawer;
---
+++ src/components/icons/IconDropdown.tsx
@@ -0,0 +1,7 @@
+const IconDropdown = () => {
+  return <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.22852 14.4932C9.6285 14.9782 10.3714 14.9782 10.7714 14.4933L14.8363 9.56487C15.3742 8.91263 14.9103 7.92859 14.0648 7.92859H5.93512C5.08965 7.92859 4.6257 8.91263 5.16366 9.56487L9.22852 14.4932Z" fill="#FBE5FC" />
+  </svg>
+}
+
+export default IconDropdown;
