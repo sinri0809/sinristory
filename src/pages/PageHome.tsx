268282e59f247b45f2fbe450342737fde3a2791d
sinri <1lliodqb@gmail.com>
2023-01-24 21:19:07 +0900

    feat: Add home Popup  - scss: Add background color var

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Popup from "components/layout/Popup";
+
 const PageHome = () => {
   return (
     <main className="home">
-      hello sinri
+      <Popup>
+        <div className="popup-home-welcome">
+          <Popup.Content>
+            <strong className="emoji">️🏄‍♀️</strong>
+            <h1>Welcome sinri’s sea !</h1>
+            <p>
+              I hope you enjoy surfing my UX <br />
+              Let’s surfing
+            </p>
+          </Popup.Content>
+          <Popup.Controller>
+            버튼
+          </Popup.Controller>
+        </div>
+      </Popup>
     </main>
   );
 }
