5f13b23dd12dbc9c9ad01fe98cd5361eb583423a
sinri <1lliodqb@gmail.com>
2023-02-26 19:22:13 +0900

    feat: Add Device - phone check

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import MainRouter from 'routes/Router';
 import Header from 'components/layout/Header';
 import BackgroundSea from 'components/common/BackgroundSea';
 
+import { isPhone } from 'tools/devices';
+
 import './styles/index.scss';
 
 function App() {
   useEffect(() => {
     document.body.classList.add('light');
+    console.log(`isPhone? ${isPhone}`)
   }, [])
 
   return (
---
+++ src/tools/devices.ts
@@ -0,0 +1,4 @@
+// const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+const isPhone = /iPhone|Android/i.test(navigator.userAgent);
+
+export { isPhone };
