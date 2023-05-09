f7a222b8d7766912926fa586fde761a05a259662
SujinJeong <sujin_jeong3@tmax.co.kr>
2023-05-09 18:23:37 +0900

    refactor: move directory, delete unused

--- src/components/container/Board.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function Board({ children }: Props) {
-  return <div className="board__container">{children}</div>;
-}
