5e1ca2676cc9e2bdaf502c086a3081c9e59e4b39
sinri <1lliodqb@gmail.com>
2023-05-29 19:25:39 +0900

    chore: organizing consoles

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import Header from 'components/layout/Header';
 import BackgroundSea from 'components/common/BackgroundSea';
 
 import { isPhone } from 'tools/devices';
-import { css } from '@emotion/react';
+// import { css } from '@emotion/react';
 import './styles/index.scss';
 
 function App() {
   useEffect(() => {
     document.body.classList.add('light');
-    console.log(`isPhone? ${isPhone}`);
-    window.scrollTo({
-      top: 0,
-      // behavior: 'instant',
-    });
+    console.debug(`${isPhone ? "mobile" : "pc"}`);
+    // window.scrollTo({
+    //   top: 0,
+    //   // behavior: 'instant',
+    // });
   }, []);
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -16,13 +16,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <RecoilRoot>
       <Router>
         <App />
       </Router>
     </RecoilRoot>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
