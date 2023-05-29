e0587c59dfbc120c5eacfdbfd2e93136bc9b0cfe
sinri <1lliodqb@gmail.com>
2023-05-29 20:48:28 +0900

    fix: add new wave paths

--- src/components/common/BackgroundSea.tsx
+++ src/components/common/BackgroundSea.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
 
 const BackgroundSea = () => {
-  return <div className="background-sea"></div>;
+  return <div className="background-sea">
+    <svg className='waves' width="1578" height="1464" viewBox="0 0 1578 1464" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path className='wave-path-4'
+        d="M1286.74 166.221C1358.16 59.9765 1493.04 15.4165 1577 0H0V1460C127.988 1242.94 245.716 1256.11 358.253 1216.1C443.388 1185.82 547.045 1121.12 607.374 899.077C645.071 760.328 762.836 656.693 883.029 617.231C999.547 578.975 1085.12 606.049 1178.89 505.89C1286.74 390.704 1208.79 282.175 1286.74 166.221Z" fill="url(#paint0_linear_455_1611)" />
+      <g filter="url(#filter0_d_455_1611)">
+        <path className='wave-path-3' d="M1126 145.5C1188.5 52.5 1306.53 13.4947 1380 0H0V1278C112 1088 215.021 1099.53 313.5 1064.5C388 1038 478.707 981.367 531.5 787C564.488 665.547 667.542 574.831 772.72 540.289C874.683 506.802 949.564 530.5 1031.63 442.827C1126 342 1057.79 247 1126 145.5Z" fill="url(#paint1_linear_455_1611)" shapeRendering="crispEdges" />
+      </g>
+      <g filter="url(#filter1_d_455_1611)">
+        <path className='wave-path-2' d="M370.756 290.537C155.135 355.192 33.7434 523.785 0 600V0H720C670.018 101.79 640.281 209.719 370.756 290.537Z" fill="url(#paint2_linear_455_1611)" fillOpacity="0.2" shapeRendering="crispEdges" />
+        {/* <path d="M370.756 290.537C155.135 355.192 33.7434 523.785 0 600V0H720C670.018 101.79 640.281 209.719 370.756 290.537Z" stroke="white" strokeOpacity="0.2" shapeRendering="crispEdges" /> */}
+      </g>
+      <g filter="url(#filter2_d_455_1611)">
+        <path className='wave-path-1' d="M0 300V0H300C245.813 199.859 77.4221 283.275 0 300Z" fill="url(#paint3_linear_455_1611)" fillOpacity="0.2" shapeRendering="crispEdges" />
+        {/* <path d="M0 300V0H300C245.813 199.859 77.4221 283.275 0 300Z" stroke="white" strokeOpacity="0.2" shapeRendering="crispEdges" /> */}
+      </g>
+      <defs>
+        <filter id="filter0_d_455_1611" x="-20" y="-20" width="1428" height="1326" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset dx="4" dy="4" />
+          <feGaussianBlur stdDeviation="12" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_455_1611" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_455_1611" result="shape" />
+        </filter>
+        <filter id="filter1_d_455_1611" x="-8.5" y="-8.5" width="745.303" height="624.702" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset dx="4" dy="4" />
+          <feGaussianBlur stdDeviation="6" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_455_1611" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_455_1611" result="shape" />
+        </filter>
+        <filter id="filter2_d_455_1611" x="-0.5" y="-0.5" width="317.154" height="317.12" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset dx="8" dy="8" />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.02 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_455_1611" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_455_1611" result="shape" />
+        </filter>
+        <linearGradient id="paint0_linear_455_1611" x1="0" y1="0" x2="1453.5" y2="1453.5" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" stopOpacity="0" />
+          <stop offset="0.86024" stopColor="white" stopOpacity="0.481734" />
+          <stop offset="1" stopColor="white" stopOpacity="0.56" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_455_1611" x1="0" y1="0" x2="1169.5" y2="1169.5" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" stopOpacity="0.64" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient id="paint2_linear_455_1611" x1="360" y1="0" x2="360" y2="600" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0.3" />
+        </linearGradient>
+        <linearGradient id="paint3_linear_455_1611" x1="188.617" y1="192" x2="-3.20159" y2="8.34389" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0.6" />
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>;
 };
 
 export default BackgroundSea;
